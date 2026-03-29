753704858f047c64609169d31dab839d73e93996
Robert Bell <robert.bell@hashicorp.com>
2026-03-28 20:58:11 -0400

    Removed unneeded files

--- .config/fish/functions/nl.fish
+++
@@ -1,4 +0,0 @@
-function nl
-    set -gx NOMAD_ADDR https://$argv[1].it-nomad.hashicorp.services:4646
-    set -gx NOMAD_TOKEN (nomad login -method=Okta --json | jq -r '.SecretID')
-end
--- .config/fish/functions/vl.fish
+++
@@ -1,4 +0,0 @@
-function vl
-    set -gx VAULT_ADDR https://$argv[1].it-vault.hashicorp.services
-    vault login -method=oidc -path=it-engineering-cloud-oidc role=it-engineering-cloud
-end
